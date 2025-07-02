60b0d533e9383cf1119b5935b4228cc08ec401d5
Tail-R <tr.wx58@gmail.com>
2025-07-02 23:30:27 +0900

    Performed a clean build of LuaJIT

--- src/game_server/game_server.hpp
+++ src/game_server/game_server.hpp
@@ -18,7 +18,7 @@ private:
     void accept_loop();
     void handle_client();
 
-    ServerSocket        m_server_socket;
+    // ServerSocket        m_server_socket;
     std::thread         m_main_thread;
     std::atomic<bool>   m_running;
 };
